90de8601f08a94528d7ed80b0c30dc2391d19f27
shanghua.zhao <shanghua.zhao@huanxi.fm>
2017-10-26 17:48:42 +0800

    no message

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+/**
+ * 这是一个普通的组件
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'my-app',    // 组件名称
+    templateUrl: './app/app.component.html'    // 组件模板
+})
+
+export class AppComponent implements OnInit {
+    private greeting:string;
+    constructor() { }
+
+    ngOnInit() {
+        this.greeting = 'Angular 2组件 ';
+    }
+}
